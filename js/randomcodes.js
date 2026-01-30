d15d002b59bdf1a2adad285c3f59b285f25b8909
Nicoleossai123 <n.ossai.366@westcliff.edu>
2026-01-30 15:15:17 -0800

    randomcode

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,7 +1,10 @@
-function generateCode() {
-    var code = "";
-    var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var code = "";
+var getCode = "";
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+var btnvalue = true;
 
+function generateCode() {
+    code = "";
     for (var i = 1; i <= 8; i++) {
         var char = Math.floor(Math.random() * str.length);
         code += str.charAt(char);
@@ -11,8 +14,31 @@ function generateCode() {
 
 document.getElementById("codes").innerHTML = generateCode();
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    var submitBtn = document.getElementById("submit");
+
+    if (btnvalue === true) {
+        submitBtn.disabled = true;
+        submitBtn.style.backgroundColor = "#cccccc";
+        submitBtn.style.cursor = "not-allowed";
+    } else {
+        submitBtn.disabled = false;
+        submitBtn.style.backgroundColor = "#4977D1";
+        submitBtn.style.cursor = "pointer";
+    }
 }
 
-disableButton();
+document.getElementById("randomcode").addEventListener("keyup", evaluateCode);
+
+function evaluateCode() {
+    getCode = document.getElementById("randomcode").value;
+
+    var cleanInput = getCode.trim();
+    var cleanCode = code.trim();
+
+    if (cleanInput.length === 8 && cleanInput === cleanCode) {
+        disableButton(false);
+    } else {
+        disableButton(true);
+    }
+}
